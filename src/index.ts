175e1715d33bfae696edeb1d63c79089f8f9e4a2
George Payne <georgepayne@hotmail.com>
2020-07-22 16:21:51 +0200

    add lint command

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export class Credentials {
   public username: string;
   public password: string;
 
-  constructor(username: string, password: string) {
+  constructor(username:     string, password: string) {
     this.username = username;
     this.password = password;
   }
