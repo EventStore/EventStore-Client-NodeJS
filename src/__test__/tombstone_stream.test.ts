19573e7472b43c959077f9cdc706479119988f00
YoEight <yo.eight@gmail.com>
2020-08-14 16:36:19 +0200

    Implement delete and tombstone commands.

---
+++ src/__test__/tombstone_stream.test.ts
@@ -0,0 +1,33 @@
+// import { v4 as uuid } from "uuid";
+
+import * as eventstore from "../";
+import { EventData, Revision } from "../types";
+import {v4 as uuid} from "uuid";
+
+describe("tombstone_stream", function () {
+  it("should successfully tombstone a stream", async () => {
+    const connection = eventstore.EventStoreConnection.builder().build(
+      "localhost:2113"
+    );
+
+    const streamName = `tombstone-${uuid()}`;
+    const evt = EventData.json("typescript-type", {
+      message: "baz",
+    }).build();
+
+    await connection
+        .streams()
+        .writeEvents(streamName)
+        .expectedVersion(Revision.Any)
+        .send([evt])
+
+    const result = await connection
+        .streams()
+        .tombstone(streamName)
+        .execute();
+
+    console.log(result);
+
+    expect(1).toBe(1);
+  });
+});
