f1130dbfa340198c80107c57d2698c882638806a
YoEight <yo.eight@gmail.com>
2020-08-24 17:03:35 +0200

    Implement cluster connection.

---
+++ src/gossip.ts
@@ -0,0 +1,58 @@
+import {GossipClient} from "../generated/gossip_grpc_pb";
+import * as grpc from 'grpc';
+import {EndPoint, GossipSeed, MemberInfo} from "./types";
+import {Empty} from "../generated/shared_pb";
+
+export class Gossip {
+    private readonly _client: GossipClient;
+
+    constructor(uri: string, cert?: Buffer) {
+        let creds: grpc.ChannelCredentials;
+
+        if (cert) {
+            creds = grpc.credentials.createSsl(cert);
+        } else {
+            creds = grpc.credentials.createInsecure();
+        }
+
+        this._client = new GossipClient(uri, creds);
+    }
+
+    static fromSeed(seed: GossipSeed, cert?: Buffer): Gossip {
+        return new Gossip(`${seed.hostname}:${seed.port}`, cert);
+    }
+
+    list(): Promise<MemberInfo[]> {
+        return new Promise((resolve, reject) => {
+            this._client.read(new Empty(), (error, info) => {
+               if (error) reject(error);
+
+               const members: MemberInfo[] = [];
+
+               for (const grpcMember of info.getMembersList()) {
+                    let httpEndpoint;
+                    let grpcHttpEndpoint = grpcMember.getHttpEndPoint();
+
+                    if (grpcHttpEndpoint) {
+                        httpEndpoint = {
+                            address: grpcHttpEndpoint.getAddress(),
+                            port: grpcHttpEndpoint.getPort(),
+                        };
+                    }
+
+                    let member: MemberInfo = {
+                        instanceId: grpcMember.getInstanceId()?.getString(),
+                        timeStamp: grpcMember.getTimeStamp(),
+                        state: grpcMember.getState(),
+                        isAlive: grpcMember.getIsAlive(),
+                        httpEndpoint,
+                    };
+
+                    members.push(member);
+               }
+
+               resolve(members);
+            });
+        });
+    }
+}
