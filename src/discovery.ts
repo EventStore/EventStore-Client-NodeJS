7cde883d00a23ba4834e8698f1e6896824c8d93d
YoEight <yo.eight@gmail.com>
2020-08-25 00:25:31 +0200

    Formatting.

--- src/discovery.ts
+++ src/discovery.ts
@@ -1,128 +1,138 @@
-import {ClusterSettings, EndPoint, GossipSeed, MemberInfo, NodePreference} from "./types";
-import * as dns from 'dns';
-import {Gossip} from "./gossip";
-import {MemberInfo as GrpcMemberInfo} from "../generated/gossip_pb";
+import {
+  ClusterSettings,
+  EndPoint,
+  GossipSeed,
+  MemberInfo,
+  NodePreference,
+} from "./types";
+import * as dns from "dns";
+import { Gossip } from "./gossip";
+import { MemberInfo as GrpcMemberInfo } from "../generated/gossip_pb";
 import VNodeState = GrpcMemberInfo.VNodeState;
 
-export function discoverEndpoint(setts: ClusterSettings, previous?: EndPoint, cert?: Buffer): Promise<EndPoint> {
-    return new Promise(async (resolve, error) => {
-        while (true) {
-            let candidates: GossipSeed[] = [];
+export async function discoverEndpoint(
+  setts: ClusterSettings,
+  previous?: EndPoint,
+  cert?: Buffer
+): Promise<EndPoint> {
+  for (;;) {
+    let candidates: GossipSeed[] = [];
 
-            if (setts.gossipSeeds) {
-                candidates = setts.gossipSeeds;
-            } else if (setts.domain) {
-                try {
-                    candidates = await resolveDomainName(setts.domain);
-                } catch {
-                    // TODO - Log about why it has failed.
-                }
-            }
+    if (setts.gossipSeeds) {
+      candidates = setts.gossipSeeds;
+    } else if (setts.domain) {
+      try {
+        candidates = await resolveDomainName(setts.domain);
+      } catch {
+        // TODO - Log about why it has failed.
+      }
+    }
 
-            for (const candidate of candidates) {
-                const client = Gossip.fromSeed(candidate, cert);
-                let members: MemberInfo[] = []
+    for (const candidate of candidates) {
+      const client = Gossip.fromSeed(candidate, cert);
+      let members: MemberInfo[] = [];
 
-                try {
-                    await client.list();
-                } catch {
-                    continue;
-                }
+      try {
+        members = await client.list();
+      } catch {
+        continue;
+      }
 
-                if (members.length !== 0) {
-                    const preference = setts.nodePreference ?? NodePreference.Random;
-                    const selected = determineBestNode(preference, members);
+      if (members.length !== 0) {
+        const preference = setts.nodePreference ?? NodePreference.Random;
+        const selected = determineBestNode(preference, members);
 
-                    if (selected) {
-                        let endpoint: EndPoint = {
-                            address: selected.address,
-                            port: selected.port,
-                        };
+        if (selected) {
+          const endpoint: EndPoint = {
+            address: selected.address,
+            port: selected.port,
+          };
 
-                        resolve(endpoint);
-                        break;
-                    }
-                }
-            }
-
-            await asyncSetTimeout(500);
+          return Promise.resolve(endpoint);
         }
-    });
+      }
+    }
+
+    await asyncSetTimeout(500);
+  }
 }
 
 function resolveDomainName(domain: string): Promise<GossipSeed[]> {
-    return new Promise(async (resolve, reject) => {
-        dns.resolveSrv(domain, (error, addresses) => {
-            if (error) reject(error);
+  return new Promise((resolve, reject) => {
+    dns.resolveSrv(domain, (error, addresses) => {
+      if (error) reject(error);
 
-            const seeds: GossipSeed[] = addresses.map((record) => {
-               return {
-                   hostname: record.name,
-                   port: record.port,
-               };
-            });
+      const seeds: GossipSeed[] = addresses.map((record) => {
+        return {
+          hostname: record.name,
+          port: record.port,
+        };
+      });
 
-            resolve(seeds);
-        });
+      resolve(seeds);
     });
+  });
 }
 
 function inAllowedStates(member: MemberInfo): boolean {
-    switch (member.state) {
-        case VNodeState.SHUTDOWN:
-            return false;
-        default:
-            return true;
-    }
+  switch (member.state) {
+    case VNodeState.SHUTDOWN:
+      return false;
+    default:
+      return true;
+  }
 }
 
 function asyncSetTimeout(timeout: number): Promise<void> {
-    return new Promise(resolve => {
-        setTimeout(() => {
-           resolve();
-        }, timeout)
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeout);
+  });
 }
 
-function determineBestNode(preference: NodePreference, members: MemberInfo[]): EndPoint | undefined {
-    let finalCandidates = [];
-    const sorted = members
-        .filter(inAllowedStates)
-        .sort((a, b) => a.state - b.state);
+function determineBestNode(
+  preference: NodePreference,
+  members: MemberInfo[]
+): EndPoint | undefined {
+  const sorted = members
+    .filter(inAllowedStates)
+    .sort((a, b) => a.state - b.state);
 
-    let finalMember;
-    switch (preference) {
-        case NodePreference.Master:
-            finalMember = sorted.find(member => member.state === VNodeState.LEADER);
-            if (finalMember && finalMember.httpEndpoint) {
-                return {
-                    address: finalMember.httpEndpoint.address,
-                    port: finalMember.httpEndpoint.port,
-                };
-            }
-            break;
+  let finalMember;
+  switch (preference) {
+    case NodePreference.Master:
+      finalMember = sorted.find((member) => member.state === VNodeState.LEADER);
+      if (finalMember && finalMember.httpEndpoint) {
+        return {
+          address: finalMember.httpEndpoint.address,
+          port: finalMember.httpEndpoint.port,
+        };
+      }
+      break;
 
-        case NodePreference.Slave:
-            finalMember = sorted.filter(member => member.state === VNodeState.FOLLOWER)
-                .sort(() => Math.random() - 0.5)
-                .shift();
+    case NodePreference.Slave:
+      finalMember = sorted
+        .filter((member) => member.state === VNodeState.FOLLOWER)
+        .sort(() => Math.random() - 0.5)
+        .shift();
 
-            if (finalMember && finalMember.httpEndpoint) {
-                return {
-                    address: finalMember.httpEndpoint.address,
-                    port: finalMember.httpEndpoint.port,
-                };
-            }
-            break;
+      if (finalMember && finalMember.httpEndpoint) {
+        return {
+          address: finalMember.httpEndpoint.address,
+          port: finalMember.httpEndpoint.port,
+        };
+      }
+      break;
 
-        default:
-            finalMember = sorted.sort(() => Math.random() - 0.5).shift();
-            if (finalMember && finalMember.httpEndpoint) {
-                return {
-                    address: finalMember.httpEndpoint.address,
-                    port: finalMember.httpEndpoint.port,
-                };
-            }
-            break;
-    }
+    default:
+      finalMember = sorted.sort(() => Math.random() - 0.5).shift();
+      if (finalMember && finalMember.httpEndpoint) {
+        return {
+          address: finalMember.httpEndpoint.address,
+          port: finalMember.httpEndpoint.port,
+        };
+      }
+      break;
+  }
 }
