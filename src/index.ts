2325e660d367d7218ab6b97ed306b31b1d59ee8f
George Payne <georgepayne@hotmail.com>
2020-09-15 16:53:52 +0200

    clean up index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * from "./command";
-export * from "./command";
 export * from "./connection";
 export * from "./events";
 export * from "./constants";
 export * from "./types";
-export { Filter } from "./utils/Filter";
+
+export * from "./utils/Filter";
 export * from "./utils/CommandError";
