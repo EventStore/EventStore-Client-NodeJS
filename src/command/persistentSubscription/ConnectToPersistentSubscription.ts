d144b6f9741679cb75f55420a54ba2da7dd968de
George Payne <georgepayne@hotmail.com>
2020-09-25 18:41:46 +0200

    Fix ack and nack - fix how ack and nack in persistent subscription - amend tests to require ack - add basic nack test

--- src/command/persistentSubscription/ConnectToPersistentSubscription.ts
+++ src/command/persistentSubscription/ConnectToPersistentSubscription.ts
@@ -16,64 +16,28 @@ import { convertGrpcEvent } from "../../utils/convertGrpcEvent";
 
 class PersistentReportImpl implements PersistentReport {
   private _duplexStream: ClientDuplexStream<ReadReq, ReadResp>;
-  private _id?: Uint8Array | string;
-  private _buffer: number;
-  private _stream: string;
-  private _group: string;
 
-  constructor(
-    buffer: number,
-    stream: string,
-    group: string,
-    duplexStream: ClientDuplexStream<ReadReq, ReadResp>
-  ) {
-    this._buffer = buffer;
+  constructor(duplexStream: ClientDuplexStream<ReadReq, ReadResp>) {
     this._duplexStream = duplexStream;
-    this._stream = stream;
-    this._group = group;
-  }
-
-  setId(value: Uint8Array | string): void {
-    this._id = value;
   }
 
   ack(ids: string[]): void {
     const req = new ReadReq();
     const ack = new ReadReq.Ack();
-    const options = new ReadReq.Options();
-    const ackedIds: UUID[] = [];
-    const identifier = new StreamIdentifier();
-    const uuidOption = new UUIDOption();
-
-    identifier.setStreamname(Buffer.from(this._stream).toString("base64"));
-    uuidOption.setString(new Empty());
-    options.setBufferSize(this._buffer);
-    options.setGroupName(this._group);
-    options.setStreamIdentifier(identifier);
-    options.setUuidOption(uuidOption);
 
     for (const id of ids) {
       const uuid = new UUID();
       uuid.setString(id);
-      ackedIds.push(uuid);
+      ack.addIds(uuid);
     }
 
-    if (this._id) ack.setId(this._id);
-    else throw "Unexpected undefined subscription id";
-
     req.setAck(ack);
-    req.setOptions(options);
-
     this._duplexStream.write(req);
   }
 
   nack(action: PersistentAction, reason: string, ids: string[]): void {
     const req = new ReadReq();
     const nack = new ReadReq.Nack();
-    const options = new ReadReq.Options();
-    const nackedIds: UUID[] = [];
-    const identifier = new StreamIdentifier();
-    const uuidOption = new UUIDOption();
 
     switch (action) {
       case "park":
@@ -90,26 +54,15 @@ class PersistentReportImpl implements PersistentReport {
         break;
     }
 
-    identifier.setStreamname(Buffer.from(this._stream).toString("base64"));
-    uuidOption.setString(new Empty());
-    options.setBufferSize(this._buffer);
-    options.setGroupName(this._group);
-    options.setStreamIdentifier(identifier);
-    options.setUuidOption(uuidOption);
-
     for (const id of ids) {
       const uuid = new UUID();
       uuid.setString(id);
-      nackedIds.push(uuid);
+      nack.addIds(uuid);
     }
-
-    if (this._id) nack.setId(this._id);
-    else throw "Unexpected undefined subpscription id";
 
     nack.setReason(reason);
 
     req.setNack(nack);
-    req.setOptions(options);
 
     this._duplexStream.write(req);
   }
@@ -176,12 +129,7 @@ export class ConnectToPersistentSubscription extends Command {
 
     const client = await connection._client(PersistentSubscriptionsClient);
     const stream = client.read(this.metadata, callOptions);
-    const report = new PersistentReportImpl(
-      this._bufferSize,
-      this._stream,
-      this._group,
-      stream
-    );
+    const report = new PersistentReportImpl(stream);
     stream.write(req);
     stream.on("error", (error) => {
       this._handler?.onError?.(error);
@@ -189,8 +137,6 @@ export class ConnectToPersistentSubscription extends Command {
 
     stream.on("data", (resp: ReadResp) => {
       if (resp.hasSubscriptionConfirmation()) {
-        const confirmation = resp.getSubscriptionConfirmation()!;
-        report.setId(confirmation.getSubscriptionId());
         this._handler?.onConfirmation?.();
       }
 
