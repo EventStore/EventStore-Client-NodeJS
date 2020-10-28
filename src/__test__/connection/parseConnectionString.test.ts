bb5e14f0b448d7a3915c4b97193cadd325d5b5e7
George Payne <georgepayne@hotmail.com>
2020-10-28 20:41:18 +0100

    Parse connection string - parse connection strings - test for good cases - test for bad cases - test for warning cases

---
+++ src/__test__/connection/parseConnectionString.test.ts
@@ -0,0 +1,32 @@
+import { parseConnectionString } from "../../connection/parseConnectionString";
+
+import { valid, invalid, warning } from "./connectionStringMockups";
+
+describe("connection string parser", () => {
+  describe("Should parse valid connection strings", () => {
+    test.each(valid)("%s", (connectionString, expected) => {
+      const parsed = parseConnectionString(connectionString);
+      expect(parsed).toEqual(expected);
+    });
+  });
+
+  describe("Should throw on invalid strings", () => {
+    test.each(invalid)("%s", (connectionString) => {
+      expect(() => {
+        const parsed = parseConnectionString(connectionString);
+        console.log(parsed);
+      }).toThrowErrorMatchingSnapshot();
+    });
+  });
+
+  describe("Should warn on unknown keys", () => {
+    test.each(warning)("%s", (connectionString, expected) => {
+      const warnSpy = jest.spyOn(console, "warn").mockImplementation();
+      const parsed = parseConnectionString(connectionString);
+      expect(parsed).toEqual(expected);
+      expect(warnSpy).toHaveBeenCalled();
+      expect(warnSpy.mock.calls).toMatchSnapshot();
+      warnSpy.mockRestore();
+    });
+  });
+});
