d40e33ca40c15e6a1262a3b77c47da9bf079e0ee
Vlad Goldman <ledniy@gmail.com>
2021-04-30 12:20:59 +0300

    export ChannelCredentialOptions interface (#159)
    * export ChannelCredentialOptions interface
    
    * re-export constructor args
    
    * fix lint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import "./persistentSubscription";
 import "./projections";
 import "./streams";
 
-export { Client as EventStoreDBClient } from "./Client";
+export {
+  Client as EventStoreDBClient,
+  ConnectionTypeOptions,
+  ChannelCredentialOptions,
+} from "./Client";
 export * from "./events";
 export * from "./constants";
 export * from "./types";
