16787856a5c5567dc3ca0bd86523f482c6af1def
George Payne <georgepayne@hotmail.com>
2021-06-09 10:20:20 +0200

    Deprecate ConnectionTypeOptions - add deprication comment - export members - replace internal useage with `ConnectionSettings`

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import "./streams";
 export {
   Client as EventStoreDBClient,
   ConnectionTypeOptions,
+  DNSClusterOptions,
+  GossipClusterOptions,
+  SingleNodeOptions,
   ChannelCredentialOptions,
 } from "./Client";
 export * from "./events";
