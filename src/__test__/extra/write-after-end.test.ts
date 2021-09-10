75aa47d0f9769cc77d340ed941469b609e80690f
George Payne <georgepayne@hotmail.com>
2021-09-10 16:49:44 +0200

    Prevent appendToStream from writing after the stream has closed - add test

---
+++ src/__test__/extra/write-after-end.test.ts
@@ -0,0 +1,42 @@
+import { createTestNode, delay, jsonTestEvents } from "../utils";
+
+import { EventStoreDBClient } from "../..";
+import { UnavailableError } from "../../utils";
+
+describe("write after end", () => {
+  const node = createTestNode();
+  let client!: EventStoreDBClient;
+
+  beforeAll(async () => {
+    await node.up();
+    client = new EventStoreDBClient(
+      { endpoint: node.uri },
+      { rootCertificate: node.rootCertificate },
+      { username: "admin", password: "changeit" }
+    );
+  });
+
+  afterAll(async () => {
+    await node.down();
+  });
+
+  test("Should not write after end", async (done) => {
+    const STREAM_NAME = "json_stream_name";
+    await client.appendToStream(STREAM_NAME, jsonTestEvents());
+
+    client
+      .appendToStream(STREAM_NAME, jsonTestEvents(100_000))
+      .then((result) => {
+        expect(result).toBe("unreachable");
+      })
+      .catch((e) => {
+        expect(e).toBeInstanceOf(UnavailableError);
+      });
+
+    node.killNode(node.endpoints[0]);
+
+    // wait for any unhandled rejections
+    await delay(5_000);
+    done();
+  });
+});
