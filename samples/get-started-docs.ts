b7711fba448b08b7884e984a08ef8b65bdbf2be7
Alexey Zimarev <alex@zimarev.com>
2022-11-22 13:50:00 +0100

    This fails

--- samples/get-started-docs.ts
+++ samples/get-started-docs.ts
@@ -1,5 +1,7 @@
 import {EventStoreDBClient} from "@eventstore/db-client";
 
+/*
 // region createClient
 const client = EventStoreDBClient.connectionString`{connectionString}`;
 // endregion createClient
+*/
