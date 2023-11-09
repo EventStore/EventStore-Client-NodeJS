22564925b550e3cbf710c534f483629c6d7dbe82
George Payne <georgepayne@hotmail.com>
2023-11-09 13:14:06 +0100

    re-write dns test to give better failure errors

--- src/__test__/connection/dns.test.ts
+++ src/__test__/connection/dns.test.ts
@@ -36,41 +36,31 @@ optionalDescribe(!!process.env.EVENTSTORE_CLOUD_ID)("dns discover", () => {
   ])("%s", (clientType, createClient) => {
     test("should successfully connect", async () => {
       const client = createClient();
-
-      const appendResult = await client.appendToStream(STREAM_NAME, event);
-      const readResult = await collect(
-        client.readStream(STREAM_NAME, { maxCount: 10 })
-      );
-
-      expect(appendResult).toBeDefined();
-      expect(readResult).toBeDefined();
+      await expect(
+        client.appendToStream(STREAM_NAME, event)
+      ).resolves.toBeDefined();
+      await expect(
+        collect(client.readStream(STREAM_NAME, { maxCount: 10 }))
+      ).resolves.toBeDefined();
     });
 
     describe("should connect to specified preference", () => {
       test("leader", async () => {
         const client = createClient("leader");
-        const appendResult = await client.appendToStream(
-          `${clientType}-leader-test`,
-          jsonTestEvents(),
-          { requiresLeader: true }
-        );
-
-        expect(appendResult).toBeDefined();
+        await expect(
+          client.appendToStream(`${clientType}-leader-test`, jsonTestEvents(), {
+            requiresLeader: true,
+          })
+        ).resolves.toBeDefined();
       });
 
       test("follower", async () => {
         const client = createClient("follower");
-
-        try {
-          const appendResult = await client.appendToStream(
-            `${clientType}-leader-test`,
-            jsonTestEvents(),
-            { requiresLeader: true }
-          );
-          expect(appendResult).toBe("unreachable");
-        } catch (error) {
-          expect(error).toBeInstanceOf(NotLeaderError);
-        }
+        await expect(
+          client.appendToStream(`${clientType}-leader-test`, jsonTestEvents(), {
+            requiresLeader: true,
+          })
+        ).rejects.toThrow(NotLeaderError);
       });
     });
   });
