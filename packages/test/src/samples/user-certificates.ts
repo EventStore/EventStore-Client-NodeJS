1afc0dda8d7e67848aba090f5084d4d45d45bcf2
William Chong <william-chong@outlook.com>
2024-05-27 16:34:19 +0400

    Add code samples for user certificates and opentelemetry

---
+++ packages/test/src/samples/user-certificates.ts
@@ -0,0 +1,47 @@
+import { EventStoreDBClient } from "@eventstore/db-client";
+import { createTestNode, jsonTestEvents } from "@test-utils";
+import { v4 as uuid } from "uuid";
+
+const STREAM_NAME = uuid();
+
+describe("[sample] user certificates", () => {
+  const node = createTestNode();
+
+  beforeAll(async () => {
+    await node.up();
+  });
+
+  afterAll(async () => {
+    await node.down();
+  });
+
+  test("connection string", async () => {
+    const connectionStringTemplate = `esdb://admin:changeit@{endpoint}?tls=true&userCertFile={pathToCaFile}&userKeyFile={pathToKeyFile}`;
+
+    try {
+      // region client-with-user-certificates
+      const connectionString = `esdb://admin:changeit@{endpoint}?tls=true&userCertFile={pathToCaFile}&userKeyFile={pathToKeyFile}`;
+      const client = EventStoreDBClient.connectionString(connectionString);
+      // endregion client-with-user-certificates
+
+      expect(connectionString).toBe(connectionStringTemplate);
+    } catch (error) {
+      // do nothing.
+    }
+
+    const endpoints = node.endpoints
+      .map((endpoint) => `${endpoint.address}:${endpoint.port}`)
+      .join(",");
+    let connectionStringTest = connectionStringTemplate
+      .replace("{endpoint}", endpoints)
+      .replace("{pathToCaFile}", node.certPath.admin.certPath)
+      .replace("{pathToKeyFile}", node.certPath.admin.certKeyPath);
+
+    connectionStringTest = `${connectionStringTest}&tlsCaFile=${node.certPath.root}`;
+
+    const client = EventStoreDBClient.connectionString(connectionStringTest);
+
+    const result = await client.appendToStream(STREAM_NAME, jsonTestEvents(2));
+    expect(result).toBeDefined();
+  });
+});
