80024c5e944e2805de8a27bf40c2a40e3cda9fc8
William Chong <william-chong@outlook.com>
2025-02-04 12:22:06 +0400

    Format

--- packages/db-client/src/playground.ts
+++ packages/db-client/src/playground.ts
@@ -1,24 +1,27 @@
-import {EventStoreDBClient, jsonEvent} from '.'
-import {v4} from "uuid";
+import { EventStoreDBClient, jsonEvent } from ".";
+import { v4 } from "uuid";
 
 (async () => {
-    const client = EventStoreDBClient.connectionString`esdb://localhost:2113?tls=false`
+  const client = EventStoreDBClient.connectionString`esdb://localhost:2113?tls=false`;
 
-    const streamName = v4();
+  const streamName = v4();
 
-    await client.appendToStream(streamName, jsonEvent({
-        type: 'test',
-        data: {
-            message: 'Hello, World!'
-        }
-    }))
+  await client.appendToStream(
+    streamName,
+    jsonEvent({
+      type: "test",
+      data: {
+        message: "Hello, World!",
+      },
+    })
+  );
 
-    for await (const resolved of await client.readStreamFast(streamName, {
-        fromRevision: BigInt(0),
-        direction: 'forwards',
-        maxCount: BigInt(5),
-        requiresLeader: true,
-    })) {
-        console.log(resolved);
-    }
-})()
+  for await (const resolved of await client.readStreamFast(streamName, {
+    fromRevision: BigInt(0),
+    direction: "forwards",
+    maxCount: BigInt(5),
+    requiresLeader: true,
+  })) {
+    console.log(resolved);
+  }
+})();
